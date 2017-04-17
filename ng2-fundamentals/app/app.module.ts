8a6312f7ad44f4babf4a04dc18c9fe2a1c99a4e2
FATEHUDDIN MUBASHIR <fatehuddin_mohammed@apple.com>
2017-04-16 19:50:20 -0700

    Angular 2 fundamentals "Components,Component communication"

---
+++ ng2-fundamentals/app/app.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+
+import {EventsAppComponent} from './events-app.component'
+import {EventsListComponent} from './events/events-list.component'
+import {EventThumbnailComponent} from './events/event-thumbnail.component'
+import {navBarComponent} from './nav/nav.component'
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [
+        EventsAppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+        navBarComponent
+    ],
+    bootstrap: [EventsAppComponent]
+})
+export class AppModule {
+}
---
+++ ng2-fundamentals/app/events/events-list.component.ts
@@ -0,0 +1,30 @@
+import {Component} from  '@angular/core'
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>Upcoming Angular 2 Events</h1>
+        <hr/>
+        <event-thumbnail #thumbnail  [event]="event1"></event-thumbnail>
+        <h3>{{thumbnail.someProperty}}</h3>
+        <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me some foo</button>
+    </div>
+    `
+})
+export class EventsListComponent {
+    event1 = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '04/16/2017',
+        time: '10:00 am',
+        price: 599.00,
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1057 DT',
+            city: 'San Francisco',
+            country: 'America'
+        }
+    }
+
+}
