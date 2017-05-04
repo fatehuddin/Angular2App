be2fd8bfbf33c19b8063eab9b4e86c1d27203a97
FATEHUDDIN MUBASHIR <fatehuddin_mohammed@apple.com>
2017-05-04 00:58:31 -0700

    added angular router

--- ng2-fundamentals/app/app.module.ts
+++ ng2-fundamentals/app/app.module.ts
@@ -1,19 +1,24 @@
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
-
+import { RouterModule } from '@angular/router'
 import {EventsAppComponent} from './events-app.component'
 import {EventsListComponent} from './events/events-list.component'
 import {EventThumbnailComponent} from './events/event-thumbnail.component'
 import {navBarComponent} from './nav/nav.component'
 import  {EventService} from './events/shared/events.service'
 import  {ToastrService} from './common/toastr.service'
+import  { EventDetailsComponent } from './events/event-details/event-details.component'
+import { appRoutes } from './routes'
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule,
+    RouterModule.forRoot(appRoutes)
+    ],
     declarations: [
         EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
+        EventDetailsComponent,
         navBarComponent
     ],
     providers: [EventService,
