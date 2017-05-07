279847e82809d43e5f1b44c0acb44b01948b5ee9
FATEHUDDIN MUBASHIR <fatehuddin_mohammed@apple.com>
2017-05-07 01:31:27 -0700

    passing data from one component to other(communication)

--- ng2-fundamentals/app/app.module.ts
+++ ng2-fundamentals/app/app.module.ts
@@ -10,7 +10,8 @@ import  {
     CreateEventComponent,
     EventRouteActivator,
     EventListResolver,
-    CreateSessionComponent
+    CreateSessionComponent,
+    SessionListComponent
 } from './events/index'
 
 import {EventsAppComponent} from './events-app.component'
@@ -34,7 +35,8 @@ import {AuthService } from './user/auth.service'
         navBarComponent,
         CreateEventComponent,
         Error404Component,
-        CreateSessionComponent
+        CreateSessionComponent,
+        SessionListComponent
     ],
     providers: [EventService,
         ToastrService,
