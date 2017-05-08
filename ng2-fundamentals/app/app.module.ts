066d6bf6bd15714231c7e143f28629e6720033c5
FATEHUDDIN MUBASHIR <fatehuddin_mohammed@apple.com>
2017-05-07 17:56:09 -0700

    content projection example

--- ng2-fundamentals/app/app.module.ts
+++ ng2-fundamentals/app/app.module.ts
@@ -17,6 +17,7 @@ import  {
 import {EventsAppComponent} from './events-app.component'
 import {navBarComponent} from './nav/nav.component'
 import  {ToastrService} from './common/toastr.service'
+import  {CollapsibleWellComponent } from './common/collapsible-well.component'
 import {appRoutes} from './routes'
 import {Error404Component} from './errors/404.component'
 import {AuthService } from './user/auth.service'
@@ -36,7 +37,8 @@ import {AuthService } from './user/auth.service'
         CreateEventComponent,
         Error404Component,
         CreateSessionComponent,
-        SessionListComponent
+        SessionListComponent,
+        CollapsibleWellComponent
     ],
     providers: [EventService,
         ToastrService,
--- ng2-fundamentals/app/events/shared/events.service.ts
+++ ng2-fundamentals/app/events/shared/events.service.ts
@@ -22,7 +22,7 @@ export class EventService {
         EVENTS.push(event)
     }
 
-    updateEvent(event){
+    updateEvent(event) {
         let index = EVENTS.findIndex(x => x.id = event.id)
         EVENTS[index] = event
     }
